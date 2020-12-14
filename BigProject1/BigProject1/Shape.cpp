cade82b9bb6321adf14ee6bfcfab689798da829d
Amadeus <gaozhenfeng1999@gmail.com>
2020-12-14 15:23:21 +0800

    增加Shape类

---
+++ BigProject1/BigProject1/Shape.cpp
@@ -0,0 +1,85 @@
+﻿#include "Shape.h"
+
+Shape::Shape()
+{
+	filled = false;
+	boderColor = nullptr;
+	fillColor = nullptr;
+}
+
+Shape::Shape(Color boderColor, Color fillColor)
+{
+	this->boderColor = new Color{ boderColor };
+	this->fillColor = new Color{ fillColor };
+	filled = true;
+}
+
+Shape::Shape(Color boderColor)
+{
+	this->boderColor = new Color{ boderColor };
+	filled = false;
+}
+
+Shape::~Shape()
+{
+	delete boderColor;
+	boderColor = nullptr;
+	delete fillColor;
+	fillColor = nullptr;
+}
+
+bool Shape::getFilled()
+{
+	return filled;
+}
+
+void Shape::getFilledColor()
+{
+	fillColor->getColor();
+}
+
+void Shape::getBoderColor()
+{
+	boderColor->getColor();
+}
+
+int Shape::getBoderRed()
+{
+	return boderColor->getRed();
+}
+
+int Shape::getBoderGreen()
+{
+	return boderColor->getGreen();
+}
+
+int Shape::gerBoderBlue()
+{
+	return boderColor->getBlue();
+}
+
+int Shape::getFilledRed()
+{
+	return fillColor->getRed();
+}
+
+int Shape::getFilledGreen()
+{
+	return fillColor->getGreen();
+}
+
+int Shape::gerFilledBlue()
+{
+	return fillColor->getBlue();
+}
+
+void Shape::setBoderColor(int r, int g, int b)
+{
+	boderColor->setColor(r, g, b);
+}
+
+void Shape::setFilledColor(int r, int g, int b)
+{
+	boderColor->setColor(r, g, b);
+	filled = true;
+}
---
+++ BigProject1/BigProject1/Shape.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "Color.h"
+class Shape
+{
+public:
+	Shape();
+	Shape(Color boderColor, Color fillColor);
+	Shape(Color boderColor);
+	~Shape();
+	void getFilledColor();
+	void getBoderColor();
+
+	bool getFilled();
+
+	int getBoderRed();//获取边缘颜色
+	int getBoderGreen();
+	int gerBoderBlue();
+
+	int getFilledRed();//获取填充颜色
+	int getFilledGreen();
+	int gerFilledBlue();
+
+	void setBoderColor(int r, int g, int b);
+	void setFilledColor(int r, int g, int b);
+
+	virtual void draw() = 0;
+
+private:
+	Color* boderColor;
+	Color* fillColor;
+	bool filled;
+};
